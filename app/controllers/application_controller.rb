ea854773b5bef65d8ab9f11a81ec3a16d66ffff3
yoppi <y.hirokazu@gmail.com>
2011-04-30 07:47:20 +0900

    menuを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :load_menu
+
+private
+  # TODO: generate from configuration file
+  def load_menu
+    @menus = %w[blog twitter github me]
+  end
 end
