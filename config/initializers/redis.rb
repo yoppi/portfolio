3c3948b472a6d1a5aac3665cbd6043ca723293c6
yoppi <y.hirokazu@gmail.com>
2013-04-11 23:52:38 +0900

    fix typo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,7 +4,7 @@ if ENV['REDISTOGO_URL']
   uri = URI.parse(ENV['REDISTOGO_URL'])
   $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
-  raise RuntimeError, "could not find config/redis.yml" unless File.exists(Rails.root + 'config/redis.yml')
+  raise RuntimeError, "could not find config/redis.yml" unless File.exists?(Rails.root + 'config/redis.yml')
   config = YAML.load_file(Rails.root + 'config/redis.yml')
   $redis = Redis.new(:host => config.host, :port => config.port)
 end
