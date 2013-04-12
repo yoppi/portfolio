43527d8d634dfa713dc64a99deff1313494ae50c
yoppi <y.hirokazu@gmail.com>
2013-04-12 20:34:02 +0900

    add config/redis.yml

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,14 @@
-# Redis settings
-# when on heorku using environmemt variables, development use config/redis.yml
+# when on heorku use environmemt variables, development use config/redis.yml
+def redis_conf
+  File.join Rails.root, 'config/redis.yml'
+end
+
 if ENV['REDISCLOUD_URL']
   uri = URI.parse(ENV['REDISCLOUD_URL'])
   $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
-  raise RuntimeError, "could not find config/redis.yml" unless File.exists?(Rails.root + 'config/redis.yml')
-  config = YAML.load_file(Rails.root + 'config/redis.yml')
+  raise RuntimeError, "could not find config/redis.yml" unless File.exists?(redis_conf)
+  config = YAML.load_file(redis_conf)["development"]
   $redis = Redis.new(:host => config.host, :port => config.port)
 end
 
